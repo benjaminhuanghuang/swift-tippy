bb8356890da6b4d61c6aac18e22bc5481096fe84
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-02-28 23:31:04 -0800

    view controller lifecycle

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -24,6 +24,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+    
     @IBAction func onTap(_ sender: Any) {
         
         view.endEditing(true);
